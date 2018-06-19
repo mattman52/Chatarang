3b770dc8996f3d95bddcb430ee75d2ae3ef81d19
Matthew Cummings <mfcummin@iu.edu>
2018-06-18 23:24:51 -0400

    Added Class Work

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import {BrowesrRouter as Router, Route} from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDom.render (
-    <Router>
-        <Route component={App} /> ,
-    </Router>,
-    document.getElementById('root')
+ReactDOM.render(
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById('root')
 )
 registerServiceWorker()
